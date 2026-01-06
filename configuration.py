7f185c6bd1a53e5f7e0b9c6cc3099ebf9b1957f6
Adrian Montiel <adrimonty21@gmail.com>
2026-01-06 10:11:33 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-d7f98be9-9db5-4dd3-9067-6adf5abb15bd.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
